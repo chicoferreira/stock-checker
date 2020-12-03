8f36b0dfae1c19a1b1b218bb0f3a61d1f883a406
chico ferreira <franciscomferreira2311@gmail.com>
2020-12-03 21:22:48 +0000

    feat: add product properties

--- src/main/kotlin/com/github/chicoferreira/stockchecker/logger/ConsoleColor.kt
+++ src/main/kotlin/com/github/chicoferreira/stockchecker/logger/ConsoleColor.kt
@@ -9,16 +9,16 @@ enum class ConsoleColor(private val colorCode: String) {
     DARK_BLUE(ansi().a(Attribute.RESET).fg(Color.BLUE).boldOff().toString()),
     GRAY(ansi().a(Attribute.RESET).fg(Color.WHITE).boldOff().toString()),
     DARK_GRAY(ansi().a(Attribute.RESET).fg(Color.BLACK).bold().toString()),
-    GREEN(ansi().a(Attribute.RESET).fg(Color.GREEN).bold().toString()),
+    GREEN(ansi().a(Attribute.RESET).fgBright(Color.GREEN).bold().toString()),
     DARK_RED(ansi().a(Attribute.RESET).fg(Color.RED).bold().toString()),
     RED(ansi().a(Attribute.RESET).fgBrightRed().bold().toString()),
+    YELLOW(ansi().a(Attribute.RESET).fgBrightYellow().bold().toString()),
     WHITE(ansi().a(Attribute.RESET).fg(Color.WHITE).bold().toString()),
     BOLD(ansi().a(Attribute.UNDERLINE_DOUBLE).toString()),
     STRIKETHROUGH(ansi().a(Attribute.STRIKETHROUGH_ON).toString()),
     UNDERLINE(ansi().a(Attribute.UNDERLINE).toString()),
     ITALIC(ansi().a(Attribute.ITALIC).toString()),
     RESET(ansi().a(Attribute.RESET).toString());
-
 
     override fun toString(): String {
         return colorCode
---
+++ src/main/kotlin/com/github/chicoferreira/stockchecker/parser/property/ProductProperty.kt
@@ -0,0 +1,21 @@
+package com.github.chicoferreira.stockchecker.parser.property
+
+interface ProductProperty<T> {
+
+    val name: String
+
+    fun render(value: T): String
+
+    fun ofValue(value: T): Value<T> = Value<T>(this, value)
+
+    data class Value<T>(val property: ProductProperty<T>, val value: T) {
+
+        fun render(): String {
+            return property.render(value)
+        }
+
+
+    }
+
+}
+
