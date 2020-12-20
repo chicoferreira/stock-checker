5439c12e22812d203faddd28745d5dccfd348fa5
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-12-19 23:24:03 -0300

    support ktx serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
     application
     jacoco
 }
@@ -22,6 +23,7 @@ dependencies {
     implementation("org.jline:jline-terminal-jansi:3.17.1")
     implementation("org.junit.jupiter:junit-jupiter:5.4.2")
     implementation("com.google.code.gson:gson:2.8.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     testImplementation("io.mockk:mockk:1.10.3-jdk8")
 }
 
--- src/main/kotlin/com/github/chicoferreira/stockchecker/product/Product.kt
+++ src/main/kotlin/com/github/chicoferreira/stockchecker/product/Product.kt
@@ -1,14 +1,16 @@
 package com.github.chicoferreira.stockchecker.product
 
 import com.github.chicoferreira.stockchecker.StockCheckResult
+import com.github.chicoferreira.stockchecker.configuration.parser.KtxSerializationConfigurationParser
 import com.github.chicoferreira.stockchecker.parser.Website
-import com.github.chicoferreira.stockchecker.product.property.ProductProperties
+import kotlinx.serialization.Serializable
 
+@Serializable(with = KtxSerializationConfigurationParser.ProductSerializer::class)
 data class Product(val url: String) {
 
-    lateinit var website: Website
-    var latestResult: StockCheckResult? = null
+  lateinit var website: Website
+  var latestResult: StockCheckResult? = null
 
-    fun isLoaded(): Boolean = this::website.isInitialized
+  fun isLoaded(): Boolean = this::website.isInitialized
 
 }
